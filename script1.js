da848c6644e944287084d5a1de3f082b6ed22eda
2004nithin <155894331+2004nithin@users.noreply.github.com>
2024-03-20 00:50:21 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,47 @@
+let searchForm= document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let shoppingcart= document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingcart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let loginform= document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () => {
+    loginform.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+let navbar= document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+}
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
